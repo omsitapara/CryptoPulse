31a59d76ed8b95c45658cf7066285fda370e9d07
Om Sitapara <omsitapara5@gmail.com>
2026-08-18 16:17:50 +0530

    made changes to setup.py

--- dataflow/setup.py
+++ dataflow/setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="crypto-dataflow",
     version="1.0.0",
-    packages=find_packages()
+    packages=find_packages(),
+    py_modules= [
+        "config",
+        "models"
+    ]
 )
